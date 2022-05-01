f90562748e3d3602179f85ecc2360aecfb42d169
JamesNg <njames15@gmail.com>
2022-04-30 20:49:22 -0400

    commit tree bd040fb37dfcc1ee2a819388e5d63aff9da8ffd1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+//remove main#main
+const main = document.getElementById('main');
+main.remove();
+
+
+//create new header that points to 'h1#victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'JAMES is the champion';
